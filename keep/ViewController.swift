c9c1b09b8879a8cf5915e76d6d6cd9765a65af42
Matej Vancik <matej@Matejs-MacBook-Pro.local>
2015-05-29 16:03:15 +0200

    Initial Commit

---
+++ keep/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  keep
+//
+//  Created by Matej Vancik on 29/05/15.
+//  Copyright (c) 2015 Matej Vancik. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
